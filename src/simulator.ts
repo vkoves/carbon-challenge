1aa5cd08091a5219f37e55640c38fb0710795999
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2021-03-08 22:20:05 -0600

    Added basic thermometer rendering

--- src/simulator.ts
+++ src/simulator.ts
@@ -4,11 +4,24 @@
 import { GridWidth } from './constants';
 
 /**
+ * The available types of tiles
+ */
+export enum TileType {
+  Power = 'power',
+  Farm = 'farm',
+  House = 'house',
+  Office = 'office',
+  Factory = 'factory',
+  Empty = 'empty',
+}
+
+/**
  * A tile object, which contains all the info needed for rendering this tile
  * plus any of it's impacts on the total emissions
  */
 export interface ITile {
   id: number;
+  type: TileType;
 }
 
 /**
@@ -25,9 +38,25 @@ export class Simulator {
     const tiles: Array<ITile> = [];
 
     for (var i = 0; i < tilesCount; i++) {
-      tiles.push({ id: i });
+      const newTile: ITile = {
+        id: i,
+        type: TileType.Empty,
+      };
+
+      tiles.push(newTile);
     }
 
     return tiles;
+  }
+
+  /**
+   * Returns the estimate degrees of average global temperature rise. Should
+   * typically be in the range of 1 - 3.
+   */
+  public static getThermometerDegrees(
+    currentTiles: Array<ITile>
+  ): number {
+    // TODO: Make this use the current tiles to calculate stuff
+    return 1 + 2 * Math.random();
   }
 }
