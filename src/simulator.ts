f3b7665986257ef1c56c608f05fccc100341bdc7
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2021-03-09 11:42:12 -0600

    Create prototype structure for tile options

--- src/simulator.ts
+++ src/simulator.ts
@@ -16,6 +16,45 @@ export enum TileType {
   Empty = 'empty',
 }
 
+
+export interface IOption {
+  /** A percentage expressed as a decimal */
+  current: number;
+  /** A percentage expressed as a decimal */
+  target: number;
+  /** A full year (e..g '2020') */
+  targetYear: string;
+}
+
+/**
+ * Most options work something like:
+ *
+ * - CurrentValue (pulled from real-world data)
+ * - TargetValue
+ * - TargetValueYear
+ *
+ * For example, the power tile may have renewable options like so:
+ *
+ * renewable: {
+ *   current: 0,
+ *   target: 1, // 100% as a decimal
+ *   targetYear: 2050,
+ * }
+ *
+ * This means the user can impact the total warming in three ways:
+ * - Change the current value (magic mode only)
+ * - Change the target value
+ * - Change the target year
+ *
+ * A single UI option may change multiple of these at once, like if we had
+ * buttons for "100% Renewable by 2050", "50% Renewable by 2040", etc.
+ */
+export interface IOptions {
+  [TileType.Power]: {
+    renewableShare: IOption;
+  }
+}
+
 /**
  * A tile object, which contains all the info needed for rendering this tile
  * plus any of it's impacts on the total emissions
@@ -23,11 +62,11 @@ export enum TileType {
 export interface ITile {
   id: number;
   type: TileType;
+  options?: IOptions;
 }
 
 /**
  * The default layout of board tile types
- * @type {Array<TileType>}
  */
 const DefaultBoardLayout: Array<TileType> = [
     TileType.Empty, TileType.Farm, TileType.Empty, TileType.Empty,
