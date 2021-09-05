5680083500288b9832ec6b7dbc5686251d2297c6
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2021-09-05 10:32:06 -0400

    Add tooltips to Analytics emission graph

--- src/components/AnalyticsOverlay.vue
+++ src/components/AnalyticsOverlay.vue
@@ -126,6 +126,10 @@ export default class AnalyticsOverlay extends Vue {
 
   tiles: Array<TileObj> = [];
 
+  xScale: any;
+
+  graphMargins = { top: 30, right: 30, bottom: 70, left: 60 };
+
   mounted(): void {
     const currYear = Simulator.getCurrentYear();
 
@@ -138,8 +142,6 @@ export default class AnalyticsOverlay extends Vue {
 
   calculateValues(): void {
     const totalEmissionsData = Simulator.getTotalEmissionsData(this.tiles);
-
-    console.log({ 'totalEmissionsData': totalEmissionsData.data });
 
     this.graphEmissionsOverTime(totalEmissionsData.data);
 
@@ -162,34 +164,33 @@ export default class AnalyticsOverlay extends Vue {
 
   graphEmissionsOverTime(emissionsData: Array<ITotalEmissionsDatum>): void {
     // set the dimensions and margins of the graph
-    const margin = { top: 30, right: 30, bottom: 70, left: 60 };
-    const width = 800 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+    const width = 1000 - this.graphMargins.left - this.graphMargins.right;
+    const height = 300 - this.graphMargins.top - this.graphMargins.bottom;
 
     // append the svg object to the body of the page
     var svg = d3.select('#emissions-chart')
       .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
+        .attr('width', width + this.graphMargins.left + this.graphMargins.right)
+        .attr('height', height + this.graphMargins.top + this.graphMargins.bottom)
       .append('g')
         .attr('transform',
-              'translate(' + margin.left + ',' + margin.top + ')');
+              'translate(' + this.graphMargins.left + ',' + this.graphMargins.top + ')');
 
     // X axis
-    var xScale = d3.scaleBand()
-      .padding(0.2)
+    this.xScale = d3.scaleBand()
       .range([ 0, width ])
       .domain(emissionsData.map((d: ITotalEmissionsDatum) => d.year.toString()));
+
     svg.append('g')
       .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(xScale).tickValues(this.tickYears))
+      .call(d3.axisBottom(this.xScale).tickValues(this.tickYears))
       .selectAll('text')
         .attr('transform', 'translate(-10,0)rotate(-45)')
         .style('text-anchor', 'end');
 
     // Add Y axis
     var yScale = d3.scaleLinear()
-      .domain([0, 100])
+      .domain([0, 55])
       .range([ height, 0]);
     svg.append('g')
       .call(d3.axisLeft(yScale));
@@ -199,11 +200,43 @@ export default class AnalyticsOverlay extends Vue {
       .data(emissionsData)
       .enter()
       .append('rect')
-        .attr('x', (d: ITotalEmissionsDatum) => xScale(d.year.toString()) as number)
-        .attr('y', (d: ITotalEmissionsDatum) => yScale(d3.sum(Object.values(d.deltas)) + OrigYearlyEmissionsGigaTonnes))
-        .attr('width', xScale.bandwidth())
-        .attr('height', (d: ITotalEmissionsDatum) => height - yScale(d3.sum(Object.values(d.deltas)) + OrigYearlyEmissionsGigaTonnes))
-        .attr('fill', '#69b3a2');
+        .attr('class', 'bar')
+        .attr('data-year', (d: ITotalEmissionsDatum) => d.year)
+        .attr('x', (d: ITotalEmissionsDatum) => {
+          return this.xScale(d.year.toString()) as number;
+        })
+        .attr('y', (d: ITotalEmissionsDatum) => yScale(this.emissionsFromDatum(d)))
+        .attr('width', this.xScale.bandwidth())
+        .attr('height', (d: ITotalEmissionsDatum) => height - yScale(this.emissionsFromDatum(d)))
+        .on('click', this.barClick.bind(this));
+
+    d3.select('#emissions-chart')
+      .append('div')
+      .attr('class', 'tooltip');
+  }
+
+  emissionsFromDatum(datum: ITotalEmissionsDatum): number {
+    return d3.sum(Object.values(datum.deltas)) + OrigYearlyEmissionsGigaTonnes;
+  }
+
+  barClick(event: PointerEvent, datum: ITotalEmissionsDatum) {
+    const totalEmissions = this.emissionsFromDatum(datum).toFixed(2);
+
+    const tooltipOffset = 15;
+    const tooltipLeft = this.xScale(datum.year.toString())
+      + this.graphMargins.left
+      + tooltipOffset;
+
+    d3.select('.tooltip')
+      .html(`<h1>${datum.year}</h1> Emissions: ${totalEmissions} Gigatonnes`)
+      .style('left', tooltipLeft + 'px')
+      .classed('-visible', true);
+
+    d3.selectAll('.bar')
+      .classed('-active', false);
+
+    d3.select(`.bar[data-year="${datum.year}"]`)
+      .classed('-active', true);
   }
 }
 </script>
@@ -211,9 +244,55 @@ export default class AnalyticsOverlay extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import './styles/variables/colors';
 @import './styles/variables/spacing';
 
 h2 { margin-top: $standard; }
+
+figure ::v-deep {
+  position: relative;
+
+  .bar {
+    transition: fill 0.3s;
+    fill: $blue;
+    cursor: pointer;
+    stroke: $white;
+
+    &:hover { fill: $dark-blue; }
+    &.-active { fill: $dark-blue; }
+  }
+
+  .tooltip {
+    position: absolute;
+    padding: $small;
+    bottom: 100px;
+    min-width: 14rem;
+    border-radius: 0.25rem;
+    background-color: $white;
+    border: solid 1px $light-grey;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.5) ;
+    opacity: 0;
+    transition: opacity 0.3s;
+
+    &::before {
+      $tipWidth: 10px;
+
+      position: absolute;
+      display: inline-block;
+      border-top: $tipWidth solid transparent;
+      border-right: $tipWidth solid $white;
+      border-bottom: $tipWidth solid transparent;
+      border-right-color: rgb(255 255 255);
+      left: -$tipWidth;
+      top: 20px;
+      content: "";
+    }
+
+    &.-visible { opacity: 1; }
+
+    h1 { font-size: 1.25rem; }
+  }
+}
 
 dl {
   dt {
